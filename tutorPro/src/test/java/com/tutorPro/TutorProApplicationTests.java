4818ee9041bd716f1d98e3172428b38ec75904dd
Fan Yang <fanyang@fans-air.myfiosgateway.com>
2021-07-01 12:42:11 -0400

    setup springboot

---
+++ tutorPro/src/test/java/com/tutorPro/TutorProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
